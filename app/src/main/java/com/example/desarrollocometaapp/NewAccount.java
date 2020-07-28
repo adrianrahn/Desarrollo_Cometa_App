676558009048f18db99219c1b5e53c87361bad6d
adrianrahn <adrianrahn10@gmail.com>
2020-07-28 19:04:46 +0200

    Creación de Activities Login y NewAccount

---
+++ app/src/main/java/com/example/desarrollocometaapp/NewAccount.java
@@ -0,0 +1,14 @@
+package com.example.desarrollocometaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+}
