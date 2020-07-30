1ca2471c04fefdeee227889905300f830e9e18a1
adrianrahn <adrianrahn10@gmail.com>
2020-07-30 21:23:48 +0200

    Añadimos funcionalidad de lectura de Codigo QR a la app correctamente.

---
+++ app/src/main/java/com/example/desarrollocometaapp/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.desarrollocometaapp;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
--- app/src/main/java/com/example/desarrollocometaapp/MainActivity.java
+++ app/src/main/java/com/example/desarrollocometaapp/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.desarrollocometaapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.scanButton:
                 Toast.makeText(this, "scanButton pressed", Toast.LENGTH_SHORT).show();
+                ScanCode();
                 break;
             case R.id.saveButton:
                 Toast.makeText(this, "saveButton pressed", Toast.LENGTH_SHORT).show();
@@ -48,5 +54,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             default:
                 break;
         }
+    }
+
+    private void ScanCode() {
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(CaptureAct.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("scanning code");
+        integrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode, data);
+        if (result != null){
+            if (result.getContents() != null){
+
+                String resultado = result.getContents().toString();
+                quantityText.setText(resultado);
+               /* AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setMessage(result.getContents());
+                builder.setTitle("Scanning Result");
+                builder.setPositiveButton("Scan Again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ScanCode();
+                    }
+                }).setNegativeButton("Finish", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();*/
+            }
+            else {
+                Toast.makeText(this, "No Results", Toast.LENGTH_SHORT).show();
+            }
+        } else {super.onActivityResult( requestCode, resultCode, data);}
     }
 }
